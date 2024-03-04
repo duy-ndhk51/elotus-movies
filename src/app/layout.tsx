cf6b514c7296c85f322804daea3df9c1d62b4d82
Duy Nguyen <duy511998dsa@gmail.com>
2024-03-04 23:16:35 +0700

    feat: implement tab bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import '@/styles/app.scss';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import TabBar from '@/components/TabBar';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -17,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <TabBar />
+      </body>
     </html>
   );
 }
---
+++ src/components/TabBar/index.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import clsx from 'clsx';
+import dynamic from 'next/dynamic';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useCallback } from 'react';
+
+import { EMovieType } from '@/constants/movie';
+import { ClientRouting } from '@/constants/routing';
+import { useCurrentParams } from '@/hooks/useCurrentParams';
+
+import Pagination from '../Pagination';
+import styles from './styles.module.scss';
+
+const SVG = dynamic(() => import('react-inlinesvg'), { ssr: false });
+const TabBar = () => {
+  const { currentType } = useCurrentParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleRoutingToType = useCallback(
+    (type: EMovieType) => {
+      router.push(`${pathname}?type=${type}`);
+    },
+    [pathname, router],
+  );
+
+  return (
+    <div className={styles.tabBar}>
+      <button
+        type="button"
+        className={clsx(
+          styles.tabBar__button,
+          styles.tabBar__item,
+          currentType === EMovieType.TopRated && styles.active,
+        )}
+        onClick={() => {
+          handleRoutingToType(EMovieType.TopRated);
+        }}
+        title="Top Rated"
+      >
+        <SVG
+          src={`${ClientRouting.publicSVGs}/top-rated.svg`}
+          width={28}
+          height={28}
+        />
+      </button>
+      <button
+        type="button"
+        className={clsx(
+          styles.tabBar__button,
+          styles.tabBar__item,
+          currentType === EMovieType.NowPlaying && styles.active,
+        )}
+        onClick={() => {
+          handleRoutingToType(EMovieType.NowPlaying);
+        }}
+        title="Now Playing"
+      >
+        <SVG
+          src={`${ClientRouting.publicSVGs}/now-playing.svg`}
+          width={28}
+          height={28}
+        />
+      </button>
+      <button
+        className={clsx(styles.tabBar__button, styles.tabBar__item)}
+        type="button"
+      >
+        <SVG
+          src={`${ClientRouting.publicSVGs}/search.svg`}
+          width={28}
+          height={28}
+        />
+      </button>
+      <div className={styles.tabBar__item}>
+        <Pagination />
+      </div>
+    </div>
+  );
+};
+
+export default TabBar;
