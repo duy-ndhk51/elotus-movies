cf3007d7615f7e9a25993edb7b17ff3bf635868d
Duy Nguyen <duy511998dsa@gmail.com>
2024-03-05 07:59:04 +0700

    feat: implement heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { EMovieType } from '@/constants/movie';
+import { useCurrentParams } from '@/hooks/useCurrentParams';
+
+import styles from './styles.module.scss';
+
+const Heading = () => {
+  const { currentType, currentKeyword } = useCurrentParams();
+
+  return (
+    <div className={styles.heading__container}>
+      <h2 className={styles.heading}>
+        {currentType === EMovieType.All &&
+          currentKeyword &&
+          `Keyword: ${currentKeyword}`}
+        {currentType === EMovieType.NowPlaying && 'Now Playing'}
+        {currentType === EMovieType.TopRated && 'Top Rated'}
+      </h2>
+    </div>
+  );
+};
+
+export default Heading;
