1203859166f3051c227394dcd2130cfd44c3f1d5
Duy Nguyen <duy511998dsa@gmail.com>
2024-03-05 08:00:05 +0700

    refactor: support API to search keywords

--- src/pages/api/movie/index.ts
+++ src/pages/api/movie/index.ts
@@ -8,24 +8,41 @@ import errorHandler from '@/utils/error-handler';
 import { api } from '@/utils/server';
 
 async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { type, page } = req.query;
+  const { type, page, keyword } = req.query;
 
   if (
     typeof type !== 'string' ||
     !type ||
-    ![EMovieType.NowPlaying, EMovieType.TopRated].includes(type as EMovieType)
+    ![EMovieType.NowPlaying, EMovieType.TopRated, EMovieType.All].includes(
+      type as EMovieType,
+    )
   ) {
     throw new APIError('Movie type is not valid', 400);
   }
 
+  if (type === EMovieType.All && typeof keyword !== 'string') {
+    throw new APIError('Enter the keyword', 400);
+  }
+
   if (req.method === 'GET') {
+    if (type === EMovieType.All && typeof keyword === 'string') {
+      const { data } = await api.get<MoviesResponse>(`/search/movie`, {
+        params: {
+          page: typeof page === 'string' ? page : 1,
+          language: ELanguage.EN,
+          query: keyword,
+          include_adult: false,
+        },
+      });
+
+      return res.status(200).json(data);
+    }
     const { data } = await api.get<MoviesResponse>(`/movie/${type}`, {
       params: {
         page: typeof page === 'string' ? page : 1,
         language: ELanguage.EN,
       },
     });
-
     return res.status(200).json(data);
   }
 
